8a635ca02c917ec4f62f5e51150b3ed8a20a8f0d
surmach <maximsurmach@gmail.com>
2021-12-09 01:55:37 +0300

    Create an "Employee" dto and object mapper for him.
    Object mapper is created for creating "Employee" from a database query results.

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -1,7 +1,69 @@
 package com.mastery.java.task.dto;
 
+import java.time.LocalDate;
+
 public class Employee {
     private Long employeeId;
     private String firstName;
+    private String lastName;
     private Gender gender;
+    private Long departmentId;
+    private String jobTitle;
+    private LocalDate dateOfBirth;
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
