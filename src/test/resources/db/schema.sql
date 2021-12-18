110c0552089065432cd18a842c38c599666df11f
m_surmach <maximsurmach@gmail.com>
2021-12-18 16:25:51 +0300

    Set the "NOT NULL" restriction to all columns.

--- src/test/resources/db/schema.sql
+++ src/test/resources/db/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE employee
     employee_id   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name    VARCHAR(255) NOT NULL,
     last_name     VARCHAR(255) NOT NULL,
-    department_id SMALLINT,
-    job_title     VARCHAR(255),
+    department_id SMALLINT     NOT NULL,
+    job_title     VARCHAR(255) NOT NULL,
     gender        VARCHAR(6)   NOT NULL,
     date_of_birth DATE         NOT NULL
 );
