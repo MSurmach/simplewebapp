3db2654b78cfd074914ec159cd05f46df0159b95
m_surmach <maximsurmach@gmail.com>
2021-12-18 18:54:48 +0300

    Add resources improvements.

--- src/main/resources/db/V1_Database_tables_creation.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employee (
-    employee_id SMALLSERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    department_id SMALLINT,
-    job_title VARCHAR(255),
-    gender VARCHAR(6) NOT NULL,
-    date_of_birth DATE NOT NULL
-)
---
+++ src/main/resources/db/migration/V1.2_Fill_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ('Maxim', 'Surmach', 2, 'Java Developer', 'MALE', '1994-08-17');
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ('Jack', 'Daniels', 1, 'Showman', 'MALE', '1997-03-25');
+INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
+VALUES ('Matti', 'Karttunen', 3, 'Secretary', 'FEMALE', '1999-01-24');
