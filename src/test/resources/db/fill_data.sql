e8ceb26523c2f79860bbb9e2257fd9bbe675661e
Maxim Surmach <maximsurmach@gmail.com>
2022-01-20 00:21:37 +0300

    Delete all tests

--- src/test/resources/db/fill_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
-VALUES ('Maxim', 'Surmach', 2, 'Java Developer', 'MALE', '1994-08-17');
-INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
-VALUES ('Jack', 'Daniels', 1, 'Showman', 'MALE', '1997-03-25');
-INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth)
-VALUES ('Matti', 'Karttunen', 3, 'Secretary', 'FEMALE', '1999-01-24');
