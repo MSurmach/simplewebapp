7c290b9b734904352514f19d900d25d457e9410a
Maxim Surmach <maximsurmach@gmail.com>
2022-01-22 23:58:50 +0300

    Refactor. - Eliminating of a "ResponseEntity" returning. - Javadocs was deleted. - Adding new controllers.

--- src/main/java/com/mastery/java/task/rest/EmployeeController.java
+++ src/main/java/com/mastery/java/task/rest/EmployeeController.java
@@ -1,94 +1,64 @@
 package com.mastery.java.task.rest;
 
 import com.mastery.java.task.dto.Employee;
-import com.mastery.java.task.exceptions.ResourceIsAlreadyExistedException;
 import com.mastery.java.task.exceptions.ResourceIsNotFoundException;
 import com.mastery.java.task.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/**
- * The controller receives HTTP requests and transfers them to the appropriate handler to process requests.
- */
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    /**
-     * Setter for autowiring dependency.
-     *
-     * @param employeeService
-     */
     @Autowired
     public void setEmployeeService(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
 
-    /**
-     * The handler for getting all available employees from the database.
-     *
-     * @return {@link ResponseEntity} - response with HTTP code status and a body that will contain a collection of all employees.
-     */
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Employee>> allEmployees() {
-        return new ResponseEntity<>(employeeService.allEmployees(), HttpStatus.OK);
+    @GetMapping
+    public List<Employee> allEmployees() {
+        return employeeService.allEmployees();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(params = {"firstName"})
+    public List<Employee> findEmployeeByFirstName(@RequestParam String firstName) {
+        return employeeService.findEmployeeByFirstName(firstName);
+    }
+
+    @GetMapping(params = {"lastName"})
+    public List<Employee> findEmployeeByLastName(@RequestParam String lastName) {
+        return employeeService.findEmployeeByLastName(lastName);
+    }
+
+    @GetMapping(params = {"firstName", "lastName"})
+    public List<Employee> findEmployeeByFirstNameAndLastName(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.findEmployeeByFirstNameAndLastName(firstName, lastName);
+    }
+
+    @GetMapping(value = "/{id}")
     public Employee findOneEmployeeById(@PathVariable Long id) throws ResourceIsNotFoundException {
         return employeeService.findOneEmployeeById(id);
     }
 
-    /**
-     * The handler for saving a new employee.
-     *
-     * @param employee a new {@link Employee} instance that needs to be saved
-     * @return {@link ResponseEntity} response with HTTP code status, that will be 200 ("OK") if an employee is saved, or 400 ("BAD_REQUEST"), if the employee is not saved.
-     */
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HttpStatus> saveNewEmployee(@RequestBody Employee employee) {
-        try {
-            employeeService.saveNewEmployee(employee);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (ResourceIsAlreadyExistedException resourceIsAlreadyExistedException) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee saveNewEmployee(@RequestBody Employee employee) {
+        return employeeService.saveNewEmployee(employee);
     }
 
-    /**
-     * The handler for updating selected employee data.
-     *
-     * @param id       of the {@link Employee} instance, that should be updated.
-     * @param employee a new {@link Employee} instance that needs to be updated.
-     * @return {@link ResponseEntity} - response with HTTP code status, that will be 200 ("OK") if an employee is updated, or 404 ("NOT_FOUND"), if the employee is not found for the update.
-     */
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HttpStatus> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
-        try {
-            employeeService.updateEmployee(id, employee);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (ResourceIsNotFoundException exception) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    @PutMapping(value = "/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) throws ResourceIsNotFoundException {
+        return employeeService.updateEmployee(id, employee);
     }
 
-    /**
-     * The handler for deleting selected employee.
-     *
-     * @param id of the employee that needs to be deleted.
-     * @return {@link ResponseEntity} - response with HTTP code status.
-     */
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
