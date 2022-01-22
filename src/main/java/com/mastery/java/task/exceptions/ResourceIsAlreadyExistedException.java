b7fa756ed7d394d53ee859a91a93612f326cd0ae
Maxim Surmach <maximsurmach@gmail.com>
2022-01-22 07:39:06 +0300

    Rename custom exceptions to more global names.

--- src/main/java/com/mastery/java/task/exceptions/ResourceIsAlreadyExistedException.java
+++ src/main/java/com/mastery/java/task/exceptions/ResourceIsAlreadyExistedException.java
@@ -3,8 +3,8 @@ package com.mastery.java.task.exceptions;
 /**
  * Custom exception, that will be thrown if the client tries to save employee, that has already been saved.
  */
-public class EmployeeIsAlreadyExisted extends Exception {
-    public EmployeeIsAlreadyExisted(String message) {
+public class ResourceIsAlreadyExistedException extends RuntimeException {
+    public ResourceIsAlreadyExistedException(String message) {
         super(message);
     }
 }
--- src/main/java/com/mastery/java/task/exceptions/ResourceIsNotFoundException.java
+++ src/main/java/com/mastery/java/task/exceptions/ResourceIsNotFoundException.java
@@ -3,9 +3,9 @@ package com.mastery.java.task.exceptions;
 /**
  * Custom exception, that will be thrown if the client tries to find employee, that is absent inside a database.
  */
-public class EmployeeIsNotFoundException extends Exception {
+public class ResourceIsNotFoundException extends Exception {
 
-    public EmployeeIsNotFoundException(String message) {
+    public ResourceIsNotFoundException(String message) {
         super(message);
     }
 }
