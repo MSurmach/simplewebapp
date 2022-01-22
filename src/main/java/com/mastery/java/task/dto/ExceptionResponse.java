5bea705660a709024a9352e7e10854f3ecdffc4f
Maxim Surmach <maximsurmach@gmail.com>
2022-01-22 07:41:02 +0300

    Create a new "DTO", which will represent exception response, if occurs.

---
+++ src/main/java/com/mastery/java/task/dto/ExceptionResponse.java
@@ -0,0 +1,49 @@
+package com.mastery.java.task.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class ExceptionResponse {
+    private String message;
+    private int code;
+    private String codeStatus;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime time;
+
+    public ExceptionResponse() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getCodeStatus() {
+        return codeStatus;
+    }
+
+    public void setCodeStatus(String codeStatus) {
+        this.codeStatus = codeStatus;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+}
