a7f76a6b73e8f598f42f419e147b79c89eae2347
Maxim Surmach <maximsurmach@gmail.com>
2022-02-18 07:51:00 +0300

    Add finding by name logic with jpql query.

--- src/main/java/com/mastery/java/task/dao/EmployeeDao.java
+++ src/main/java/com/mastery/java/task/dao/EmployeeDao.java
@@ -2,8 +2,17 @@ package com.mastery.java.task.dao;
 
 import com.mastery.java.task.dto.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+//import org.springframework.lang.Nullable;
 
-@Repository
+import java.util.List;
+
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
+    //List<Employee> findByFirstnameContainsAndLastnameContains(@Nullable String firstname, @Nullable String lastname);
+
+    @Query("SELECT e FROM Employee e " +
+            "WHERE (:firstname IS NULL OR e.firstname LIKE %:firstname%)" +
+            "AND (:lastname IS NULL OR e.lastname LIKE %:lastname%)")
+    List<Employee> findByFirstnameContainsAndLastnameContains(@Param("firstname") String firstname, @Param("lastname") String lastname);
 }
