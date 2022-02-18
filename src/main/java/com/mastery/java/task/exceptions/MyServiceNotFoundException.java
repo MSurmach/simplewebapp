eae9da7ff54ab4891196abd3a4cd18b913602ae8
Maxim Surmach <maximsurmach@gmail.com>
2022-02-18 07:47:55 +0300

    Renamed

--- src/main/java/com/mastery/java/task/exceptions/MyServiceNotFoundException.java
+++ src/main/java/com/mastery/java/task/exceptions/MyServiceNotFoundException.java
@@ -3,9 +3,9 @@ package com.mastery.java.task.exceptions;
 /**
  * Custom exception, that will be thrown if the client tries to find employee, that is absent inside a database.
  */
-public class MyServiceIsNotFoundException extends RuntimeException {
+public class MyServiceNotFoundException extends RuntimeException {
 
-    public MyServiceIsNotFoundException(String message) {
+    public MyServiceNotFoundException(String message) {
         super(message);
     }
 }
