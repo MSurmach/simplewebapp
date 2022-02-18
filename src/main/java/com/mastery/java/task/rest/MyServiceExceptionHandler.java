68ad0d185a6474aa01bd585a0fb4b0b18bf898c3
Maxim Surmach <maximsurmach@gmail.com>
2022-02-18 07:49:05 +0300

    Change logging

--- src/main/java/com/mastery/java/task/rest/MyServiceExceptionHandler.java
+++ src/main/java/com/mastery/java/task/rest/MyServiceExceptionHandler.java
@@ -2,14 +2,14 @@ package com.mastery.java.task.rest;
 
 import ch.qos.logback.classic.Logger;
 import com.mastery.java.task.dto.SimpleWebAppExceptionResponse;
-import com.mastery.java.task.exceptions.MyServiceIsNotFoundException;
+import com.mastery.java.task.exceptions.MyServiceNotFoundException;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.annotation.Priority;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
@@ -20,39 +20,35 @@ public class MyServiceExceptionHandler {
 
     private static final Logger LOG = (Logger) LoggerFactory.getLogger(MyServiceExceptionHandler.class);
 
-    @ExceptionHandler(MyServiceIsNotFoundException.class)
+    @ExceptionHandler(MyServiceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public SimpleWebAppExceptionResponse handleNotFoundException(MyServiceIsNotFoundException exception) {
-        LOG.info("Handling an exception: {}", MyServiceIsNotFoundException.class.getSimpleName());
-        LOG.debug("Exception message: {}", exception.getMessage());
-        SimpleWebAppExceptionResponse exceptionResponse = new SimpleWebAppExceptionResponse(HttpStatus.NOT_FOUND, exception);
-        LOG.info("Returning an exception response with status: {}", exceptionResponse.getHttpStatus());
-        return exceptionResponse;
+    public SimpleWebAppExceptionResponse handleNotFoundException(HttpServletRequest request, MyServiceNotFoundException exception) {
+        logExceptionWithStackTrace(request, exception);
+        return new SimpleWebAppExceptionResponse(HttpStatus.NOT_FOUND, exception);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public List<SimpleWebAppExceptionResponse> handleConstraintViolations(ConstraintViolationException exception) {
-        LOG.info("Handling violation constraints exceptions");
+    public List<SimpleWebAppExceptionResponse> handleConstraintViolations(HttpServletRequest request, ConstraintViolationException exception) {
+        logExceptionWithStackTrace(request, exception);
         List<SimpleWebAppExceptionResponse> violationResponses = new ArrayList<>();
         for (ConstraintViolation<?> violation : exception.getConstraintViolations()) {
-            LOG.info("Handling a violation constraint: {}", violation.getMessage());
             SimpleWebAppExceptionResponse exceptionResponse = new SimpleWebAppExceptionResponse(HttpStatus.BAD_REQUEST, violation.getMessage());
-            LOG.info("Convert to an exception response with status: {}", exceptionResponse.getHttpStatus());
             violationResponses.add(exceptionResponse);
         }
-        LOG.info("Returning a list of the exception responses");
         return violationResponses;
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public SimpleWebAppExceptionResponse handleAnyUncaughtException(RuntimeException exception) {
-        LOG.info("Handling the uncaught exceptions");
+    public SimpleWebAppExceptionResponse handleAnyUncaughtException(HttpServletRequest request, Throwable exception) {
         String message = "Uncaught exception. You need to see logs";
         SimpleWebAppExceptionResponse exceptionResponse = new SimpleWebAppExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR, message);
-        LOG.info("Returning an exception response with status: {}", exceptionResponse.getHttpStatus());
+        logExceptionWithStackTrace(request, exception);
         return exceptionResponse;
     }
 
+    private void logExceptionWithStackTrace(HttpServletRequest request, Throwable exception) {
+        LOG.error("Failed to request \"{}\", {} is thrown", request.getRequestURL(), exception.getClass().getSimpleName(), exception);
+    }
 }
