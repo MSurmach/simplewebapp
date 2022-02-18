13d1b4f306d7e310a2a05b4eff39a07bc8fc136a
Maxim Surmach <maximsurmach@gmail.com>
2022-02-18 07:50:21 +0300

    Change logging. Refactor finding by name logic.

--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -1,21 +1,21 @@
 package com.mastery.java.task.service;
 
 import com.mastery.java.task.dto.Employee;
-import com.mastery.java.task.exceptions.MyServiceIsNotFoundException;
+import com.mastery.java.task.exceptions.MyServiceNotFoundException;
 
 import java.util.List;
 
 public interface EmployeeService {
 
-    List<Employee> findAllEmployees() throws MyServiceIsNotFoundException;
+    List<Employee> findAllEmployees() throws MyServiceNotFoundException;
 
-    Employee findEmployeeById(Long id) throws MyServiceIsNotFoundException;
+    Employee findEmployeeById(Long id) throws MyServiceNotFoundException;
 
-    List<Employee> findEmployeesByName(String firstName, String lastName) throws MyServiceIsNotFoundException;
+    List<Employee> findEmployeesByName(String firstName, String lastName) throws MyServiceNotFoundException;
 
     Employee saveEmployee(Employee employee);
 
     Employee updateEmployee(Long id, Employee employee);
 
-    void deleteEmployee(Long id) throws MyServiceIsNotFoundException;
+    void deleteEmployee(Long id) throws MyServiceNotFoundException;
 }
